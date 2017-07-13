2e60ca12520e9749dbd31f4bbb84ca83e7a2209a
benjamin <benjamin@codebridge.nl>
2017-07-13 22:12:25 +0200

    1.1.7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit, QueryList, ViewChildren} from "@angular/core";
+import {AfterViewInit, Component, QueryList, ViewChildren} from "@angular/core";
 import {CreateNewAutocompleteGroup} from "./ng-autocomplete/classes/AutocompleteGroup";
 import {SelectedAutocompleteItem} from "./ng-autocomplete/classes/typing";
 import {NgAutocompleteComponent} from "./ng-autocomplete/ng-autocomplete.component";
@@ -15,21 +15,27 @@ export class AppComponent implements AfterViewInit {
             'Search / choose in / from list',
             'Completer with children',
             [
-                {title: 'Option 1', id: '1', children: [
+                {
+                    title: 'Option 1', id: '1', children: [
                     {title: 'Option 4', id: '1'},
                     {title: 'Option 5', id: '2'},
                     {title: 'Option 6', id: '3'},
-                ]},
-                {title: 'Option 2', id: '2', children: [
+                ]
+                },
+                {
+                    title: 'Option 2', id: '2', children: [
                     {title: 'Option 7', id: '1'},
                     {title: 'Option 8', id: '2'},
                     {title: 'Option 9', id: '3'},
-                ]},
-                {title: 'Option 3', id: '3', children: [
+                ]
+                },
+                {
+                    title: 'Option 3', id: '3', children: [
                     {title: 'Option 10', id: '1'},
                     {title: 'Option 11', id: '2'},
                     {title: 'Option 12', id: '3'},
-                ]},
+                ]
+                },
             ],
             {titleKey: 'title', childrenKey: 'children'}
         ),
@@ -80,6 +86,25 @@ export class AppComponent implements AfterViewInit {
             false
         ),
     ];
+
+    public group3 = [
+        CreateNewAutocompleteGroup(
+            'Search / choose in / from list',
+            'Lots of items',
+            this.FillArray(),
+            {titleKey: 'title', childrenKey: null},
+            ''
+        )
+    ];
+
+    FillArray() {
+        let arr = [];
+        for (let i = 0; i < 2000; i++) {
+            arr.push({title: `Option ${i}`, id: i});
+        }
+
+        return arr;
+    }
 
     constructor() {
 
