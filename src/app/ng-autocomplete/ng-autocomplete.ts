a7f49e057186eab593753c2ccb0475e05e4dfc84
benjamin <benjamin@codebridge.nl>
2017-07-15 13:31:04 +0200

    Added Highlight Pipe to module exports.

--- src/app/ng-autocomplete/ng-autocomplete.ts
+++ src/app/ng-autocomplete/ng-autocomplete.ts
@@ -1,5 +1,6 @@
 export {NgAutoCompleteModule} from "./ng-autocomplete.module";
 export {NgDropdownDirective} from "./dropdown/ng-dropdown.directive";
+export {HighlightPipe} from "./pipes/highlight";
 export {NgAutocompleteComponent} from "./ng-autocomplete.component";
 
 export * from "./classes/AutocompleteItem";
